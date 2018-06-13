d6dbe1ef43244e0b492b3d3c69f2efe47c4f6105
Zelynger <ankudadi@gmail.com>
2018-06-13 18:14:17 +0300

    Some stuff

---
+++ HelloWorldApplication/Data/HelloWorldApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorldApplication.Models
+{
+    public class HelloWorldApplicationContext : DbContext
+    {
+        public HelloWorldApplicationContext (DbContextOptions<HelloWorldApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HelloWorldApplication.Models.User> User { get; set; }
+    }
+}
--- HelloWorldApplication/Startup.cs
+++ HelloWorldApplication/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HelloWorldApplication.Models;
 
 namespace HelloWorldApplication
 {
@@ -22,6 +24,9 @@ namespace HelloWorldApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<HelloWorldApplicationContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HelloWorldApplicationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +48,7 @@ namespace HelloWorldApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=HW}/{action=Index}/{id?}");
             });
         }
     }
