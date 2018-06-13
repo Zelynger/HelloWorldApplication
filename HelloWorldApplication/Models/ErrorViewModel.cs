78ca667437b0126322844c70a447eefc45a839ec
Zelynger <ankudadi@gmail.com>
2018-06-13 12:29:27 +0300

    Добавьте файлы проекта.

---
+++ HelloWorldApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorldApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
